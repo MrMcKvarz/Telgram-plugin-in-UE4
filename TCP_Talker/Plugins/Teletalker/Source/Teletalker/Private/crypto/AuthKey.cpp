27580c1352795dcb3081aa75680b3ae7fb1006b6
DESKTOP-K9GKTAT\Allright <rakvarz@gmail.com>
2017-09-11 18:23:07 +0300

    Replaced AuthKey array with class

---
+++ TCP_Talker/Plugins/Teletalker/Source/Teletalker/Private/crypto/AuthKey.cpp
@@ -0,0 +1,71 @@
+#include "AuthKey.h"
+#include "extensions//BinaryReader.h"
+#include "extensions//BinaryWriter.h"
+#define UI UI_ST
+THIRD_PARTY_INCLUDES_START
+#include "AllowWindowsPlatformTypes.h"
+#include "openssl/sha.h"
+#include "HideWindowsPlatformTypes.h"
+THIRD_PARTY_INCLUDES_END
+#undef UI
+AuthKey::AuthKey(unsigned char * Data)
+{
+	Key.Reserve(DEFAULT_KEY_SIZE);
+	for (int32 i = 0; i < DEFAULT_KEY_SIZE; i++)
+		Key.Add(Data[i]);
+
+	unsigned char KeySHA[20];
+	SHA1((unsigned char *)Key.GetData(), DEFAULT_KEY_SIZE, KeySHA);
+	BinaryReader Reader(KeySHA, 20);
+	AuxHash = Reader.ReadLong();
+	Reader.Read(4);
+	KeyID = Reader.ReadLong();
+}
+
+AuthKey::AuthKey()
+{
+	Key.Reserve(DEFAULT_KEY_SIZE);
+	Key.Empty();
+	AuxHash = 0;
+	KeyID = 0;
+}
+
+TArray<unsigned char> AuthKey::CalculateNewNonceHash(TArray<unsigned char> Nonce, int32 Number)
+{
+	BinaryWriter Writer;
+	Writer.Write(Nonce.GetData(), Nonce.Num());
+	Writer.WriteByte((unsigned char *)(&Number));
+	Writer.WriteLong(AuxHash);
+
+	unsigned char NewNonceSHA[20];
+	SHA1(Writer.GetBytes().GetData(), Writer.GetWrittenBytesCount(), NewNonceSHA);
+
+	TArray<unsigned char> NewNonceHash1;
+	NewNonceHash1.Reserve(20);
+	for (int32 i = 4; i < 20; i++)
+		NewNonceHash1.Add(NewNonceSHA[i]);
+	return NewNonceHash1;
+}
+
+TArray<unsigned char> AuthKey::GetKey()
+{
+	return Key;
+}
+
+unsigned long long AuthKey::GetAuxHash()
+{
+	return AuxHash;
+}
+
+unsigned long long AuthKey::GetKeyID()
+{
+	return KeyID;
+}
+
+AuthKey& AuthKey::operator=(const AuthKey& Copy)
+{
+	this->Key = Copy.Key;
+	this->AuxHash = Copy.AuxHash;
+	this->KeyID = Copy.KeyID;
+	return *this;
+}
---
+++ TCP_Talker/Plugins/Teletalker/Source/Teletalker/Public/crypto/AuthKey.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Engine.h"
+class AuthKey
+{
+	TArray<unsigned char> Key;
+	unsigned long long AuxHash;
+	unsigned long long KeyID;
+	const int32 DEFAULT_KEY_SIZE = 256;
+public:
+	AuthKey();
+	AuthKey(unsigned char * Data);
+	TArray<unsigned char> CalculateNewNonceHash(TArray<unsigned char> Nonce, int32 Number);
+
+	TArray<unsigned char> GetKey();
+	unsigned long long GetAuxHash();
+	unsigned long long GetKeyID();
+
+	AuthKey& operator=(const AuthKey& other);
+
+};
